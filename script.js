927e6ec6c28fb43f1d9f412f9e02edd5d80e3ce6
Fernnog <fernandocnogueira@gmail.com>
2025-01-16 12:55:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,203 +1,238 @@
-const descricaoInput = document.getElementById('descricao');
-const valorInput = document.getElementById('valor');
-const adicionarBtn = document.getElementById('adicionar');
-const listaComprasTbody = document.getElementById('listaCompras').getElementsByTagName('tbody')[0];
-const totalValorSpan = document.getElementById('totalValor');
-const exportarBtn = document.getElementById('exportar');
-const importarBtn = document.getElementById('importar');
-const resetarBtn = document.getElementById('resetar');
-const relatorioBtn = document.getElementById('relatorio');
-const arquivoImportarInput = document.getElementById('arquivoImportar');
+/* =========================================
+    ESTILOS GERAIS
+   ========================================= */
 
-let compras = [];
-
-// Lista de sugestões para o autocomplete
-const listaSugestoes = [
-    "Refresco", "Guardanapo", "Saco de lixo 15 L", "Saco de lixo 30 L",
-    "Sabão em pó", "Amaciante", "Água sanitária", "Sabão para lavar roupas líquido",
-    "Desinfetante", "Detergente", "Sabão em barra", "Escova de dente",
-    "Pasta de dente", "Sabonete", "Shampoo", "Condicionador",
-    "Papel higiênico", "Azeitona", "Óleo de soja", "Ketchup", "Mostarda",
-    "Maionese", "Azeite", "Pipoca", "Achocolatado em pó", "Leite em pó",
-    "Batata palha", "Massa para bolo", "Macarrão espaguete",
-    "Macarrão penne", "Geleia", "Mel", "Biscoito recheado",
-    "Biscoito creme cracker", "Tomate", "Alface", "Cebola", "Laranja",
-    "Banana", "Uva", "Limão", "Manteiga", "Margarina", "Iogurte",
-    "Mortadela", "Mussarela", "Yakult", "Biscoito genérico", "Café", "Miojo"
-];
-
-// Configuração do Awesomplete para desconsiderar acentos
-new Awesomplete(descricaoInput, {
-    list: listaSugestoes,
-    filter: function(text, input) {
-        return Awesomplete.FILTER_CONTAINS(text, input.match(/[^,]*$/)[0]);
-    },
-    replace: function(text) {
-        // Pega o texto antes da última vírgula (ou todo o texto se não houver vírgula)
-        var before = this.input.value.match(/^.+,\s*|/)[0]; 
-        // Se houver texto antes (ou seja, mais de um item sendo digitado), adiciona a vírgula
-        if (before) {
-            this.input.value = before + text + ", ";
-        } else {
-            // Caso contrário, apenas substitui o texto atual pelo item selecionado
-            this.input.value = text;
-        }
-    }
-});
-
-// Adiciona um ouvinte de eventos 'awesomplete-selectcomplete' ao campo de descrição
-descricaoInput.addEventListener('awesomplete-selectcomplete', function(event) {
-    valorInput.focus(); // Move o foco para o campo de valor
-});
-
-function atualizarLista() {
-    listaComprasTbody.innerHTML = '';
-    let total = 0;
-
-    compras.forEach((compra, index) => {
-        let row = listaComprasTbody.insertRow();
-        let descricaoCell = row.insertCell();
-        let valorCell = row.insertCell();
-        let acoesCell = row.insertCell(); // Nova célula para as ações
-
-        descricaoCell.innerHTML = compra.descricao;
-        valorCell.innerHTML = compra.valor.toFixed(2);
-
-        // Cria o botão de exclusão
-        let botaoExcluir = document.createElement('button');
-        botaoExcluir.className = 'botao-excluir';
-        botaoExcluir.innerHTML = '-'; // Símbolo de "menos"
-        botaoExcluir.setAttribute('onclick', ''); // Adiciona o atributo onclick vazio
-        botaoExcluir.addEventListener('click', () => {
-            // Remove o item da lista 'compras'
-            compras.splice(index, 1);
-            // Atualiza a lista e o total
-            atualizarLista();
-        });
-
-        acoesCell.appendChild(botaoExcluir); // Adiciona o botão à célula de ações
-
-        total += compra.valor;
-    });
-
-    totalValorSpan.innerHTML = total.toFixed(2);
-    atualizarPainelTotal();
+body {
+    font-family: sans-serif;
+    background-color: #f2f2f2;
+    font-size: 1rem; /* Tamanho de fonte base */
 }
 
-function adicionarItem() {
-    const descricao = descricaoInput.value;
-    const valor = parseFloat(valorInput.value);
+/* =========================================
+    CONTAINER
+   ========================================= */
 
-    if (descricao && !isNaN(valor)) {
-        const valorFormatado = valor.toFixed(2);
-        compras.push({ descricao, valor: parseFloat(valorFormatado)});
-        atualizarLista();
-        descricaoInput.value = '';
-        valorInput.value = '';
-    } else {
-        alert('Por favor, preencha a descrição e o valor corretamente.');
-    }
+.container {
+    max-width: 95%;
+    margin: 20px auto;
+    padding: 20px;
+    background-color: #fff;
+    border-radius: 5px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
 }
 
-function exportarDados() {
-    const dataAtual = new Date().toISOString().slice(0, 10).replace(/-/g, '');
-    const nomeArquivo = `${dataAtual}_Compras no mercado.json`;
+/* =========================================
+    TÍTULOS
+   ========================================= */
 
-    const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(compras));
-    const downloadAnchor = document.createElement('a');
-    downloadAnchor.setAttribute("href", dataStr);
-    downloadAnchor.setAttribute("download", nomeArquivo);
-    document.body.appendChild(downloadAnchor);
-    downloadAnchor.click();
-    downloadAnchor.remove();
+h1, h2 {
+    color: #006400;
+    text-align: center;
+    font-size: 1.8rem; /* Títulos maiores */
 }
 
-function importarDados() {
-    arquivoImportarInput.click();
+h2 {
+    font-size: 1.4rem; /* Subtítulo um pouco menor */
 }
 
-arquivoImportarInput.addEventListener('change', function(e) {
-    const file = e.target.files[0];
-    const reader = new FileReader();
+/* =========================================
+    IMAGEM LOGO
+   ========================================= */
 
-    reader.onload = function(e) {
-        try {
-            compras = JSON.parse(e.target.result);
-            atualizarLista();
-        } catch (error) {
-            alert('Erro ao importar o arquivo.');
-        }
-    };
-
-    reader.readAsText(file);
-});
-
-function resetarDados() {
-    if (confirm('Deseja exportar os dados antes de resetar?')) {
-        exportarDados();
-    }
-
-    if (confirm('Tem certeza que deseja resetar os lançamentos?')) {
-        compras = [];
-        atualizarLista();
-    }
+header img {
+    height: 120px; /* Ajuste o tamanho conforme necessário */
+    margin-bottom: 20px; /* Adiciona uma margem abaixo do logo */
+    display: block; /* Faz o logo ocupar a linha inteira */
+    margin-left: auto;
+    margin-right: auto; /* Centraliza o logo */
 }
 
-function gerarRelatorio() {
-    if (compras.length === 0) {
-        alert('Não há dados para gerar o relatório.');
-        return;
-    }
+/* =========================================
+    GRUPOS DE ENTRADA
+   ========================================= */
 
-    const wb = XLSX.utils.book_new();
-    const ws_name = "RelatorioCompras";
-
-    const ws_data = [
-        ["Descrição", "Valor (R$)"],
-        ...compras.map(item => [item.descricao, item.valor.toFixed(2)])
-    ];
-
-    const ws = XLSX.utils.aoa_to_sheet(ws_data);
-
-    XLSX.utils.book_append_sheet(wb, ws, ws_name);
-
-    const dataAtual = new Date().toISOString().slice(0, 10).replace(/-/g, '');
-    const nomeArquivo = `${dataAtual}_RelatorioCompras.xlsx`;
-    XLSX.writeFile(wb, nomeArquivo);
+.input-group {
+    margin-bottom: 10px;
 }
 
-function atualizarPainelTotal() {
-    const totalValorPainelSpan = document.getElementById('totalValorPainel');
-    let total = 0;
-    compras.forEach(compra => {
-        total += compra.valor;
-    });
-    totalValorPainelSpan.innerHTML = total.toFixed(2);
+label {
+    display: block;
+    margin-bottom: 5px;
+    color: #333;
 }
 
-adicionarBtn.addEventListener('click', adicionarItem);
-exportarBtn.addEventListener('click', exportarDados);
-importarBtn.addEventListener('click', importarDados);
-resetarBtn.addEventListener('click', resetarDados);
-relatorioBtn.addEventListener('click', gerarRelatorio);
+/* Campos de entrada e Awesomplete aumentados em 50% */
+.input-group input[type="text"],
+.input-group input[type="number"],
+.awesomplete {
+    width: 100%;
+    padding: 8px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    box-sizing: border-box;
+    font-size: 1.875rem; /* Aumentei a fonte em 50% */
+}
 
-descricaoInput.addEventListener('keyup', function(event) {
-    if (event.key === "Enter") {
-        adicionarItem();
+/* Para aumentar o tamanho da lista de sugestões do Awesomplete */
+.awesomplete > ul {
+    font-size: 1.875rem; /* Ajuste para o mesmo tamanho do campo de entrada */
+}
+
+/* =========================================
+    BOTÕES
+   ========================================= */
+
+/* Botão "Adicionar" maior */
+.button-green.aumentado {
+    padding: 15px 30px; /* Aumentei o padding */
+    font-size: 1.5rem; /* Aumentei a fonte em 50% */
+}
+
+.button-green {
+    background-color: #4CAF50;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 1rem; /* Tamanho de fonte padrão para botões */
+}
+
+.button-gold {
+    background-color: #FFD700;
+    color: #333;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 1rem; /* Tamanho de fonte padrão para botões */
+}
+
+/* Botão de exclusão de item */
+.botao-excluir {
+    background-color: #dc3545; /* Vermelho */
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 0.8rem;
+    margin-left: 10px; /* Espaço à esquerda do botão */
+    padding: 5px 10px; /* Ajuste o padding conforme necessário */
+}
+
+/* =========================================
+    TABELA
+   ========================================= */
+
+table {
+    width: 100%;
+    border-collapse: collapse;
+    margin-top: 20px;
+}
+
+th, td {
+    padding: 8px;
+    text-align: left;
+    border-bottom: 1px solid #ddd;
+}
+
+th {
+    background-color: #008000;
+    color: white;
+}
+
+/* =========================================
+    TOTAL
+   ========================================= */
+
+.total {
+    margin-top: 20px;
+    text-align: right;
+    font-size: 18px;
+    color: #006400;
+}
+
+/* =========================================
+    BOTÕES (container)
+   ========================================= */
+
+.buttons {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 10px;
+    margin-top: 20px;
+}
+
+/* =========================================
+    PAINEL SUPERIOR
+   ========================================= */
+
+/* Painel superior aumentado em 50% */
+#painelTotal {
+    background-color: #f8f8f8;
+    border: 1px solid #ddd;
+    padding: 30px; /* Aumentei o padding */
+    margin-bottom: 20px;
+    text-align: center;
+    font-size: 3em; /* Aumentei o tamanho da fonte em 50% */
+    border-radius: 5px;
+}
+
+#painelTotal #totalValorPainel {
+    font-weight: bold;
+    color: #006400;
+}
+
+/* =========================================
+    MEDIA QUERIES (Responsividade)
+   ========================================= */
+
+@media (max-width: 768px) {
+    .container {
+        max-width: 100%;
+        padding: 10px;
     }
-});
 
-valorInput.addEventListener('keyup', function(event) {
-    if (event.key === "Enter") {
-        adicionarItem();
+    .input-group {
+        margin-bottom: 15px;
     }
-});
 
-valorInput.addEventListener('input', function(event) {
-    let valor = valorInput.value;
-    valor = valor.replace(/\D/g, '');
-    valor = valor.replace(/(\d{1,})(\d{2})$/, "$1.$2");
-    valorInput.value = valor;
-});
+    .button-green, .button-gold {
+        padding: 8px 15px;
+        font-size: 0.9rem; /* Botões um pouco menores em telas pequenas */
+    }
 
-atualizarPainelTotal();
+    /* Ajustando o tamanho do painel e campos para telas menores */
+    #painelTotal {
+        font-size: 2.4em;
+        padding: 20px;
+    }
+
+    .input-group input[type="text"],
+    .input-group input[type="number"],
+    .awesomplete {
+        font-size: 1.5rem;
+    }
+
+    .awesomplete > ul {
+        font-size: 1.5rem;
+    }
+
+    .buttons {
+        flex-direction: column;
+    }
+
+    h1 {
+        font-size: 1.5rem; /* Títulos menores em telas pequenas */
+    }
+
+    h2 {
+        font-size: 1.2rem;
+    }
+
+    /* Ajustando o botão Adicionar*/
+    .button-green.aumentado {
+        padding: 12px 24px;
+        font-size: 1.2rem;
+    }
+}
